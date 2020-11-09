d8ae3bc3bcca218ebbe3c20471192bb4f5e1defd
leon <leontimnaluithlen@gmail.com>
2020-11-09 13:25:07 +0100

    contextual epsilon greedy works as expected!

--- src/main/scala/periphery/demos/PointRegressionContextDemo.scala
+++ src/main/scala/periphery/demos/PointRegressionContextDemo.scala
@@ -1,7 +1,7 @@
 package epsilon.demos
 
 import scala.collection.mutable.{Map => MutableMap}
-import scala.collection.mutable.{ArrayBuffer => MutableList}
+import scala.collection.mutable.{ListBuffer}
 
 import epsilon.core.components.distributions.PointRegressionContext
 import epsilon.core.ensembles._
@@ -9,6 +9,7 @@ import epsilon.core.models.DummyModel
 import smile.data.DataFrame
 import smile.data.formula._
 
+import plotting.Chart
 
 object DemoPointRegressionContext{
 
@@ -31,41 +32,53 @@ object DemoPointRegressionContext{
         (context,aggregateReward, reward) => {aggregateReward.update(context, reward); aggregateReward}
     )
 
-    def run(): Unit = {
-        println("started run")
-        var i = 0
-        while(i < 1000){
-            ensemble.update(0, Array(1.0, 0.0, 1.0), 0.0)
-            ensemble.update(0, Array(1.0, 1.0, 0.0), 1.0)
-
-            ensemble.update(1, Array(1.0, 1.0, 0.0), 0.0)
-            ensemble.update(1, Array(1.0, 0.0, 1.0), 1.0)
-            i += 1
-        }
-        println("finished loop")
-
-        val (action0, modelId0) = ensemble.actWithID(Array(1.0, 1.0, 0.0), ())
-        val (action1, modelId1) = ensemble.actWithID(Array(1.0, 0.0, 1.0), ())
-        println("single action")
-        println(f"action0: $action0, modelId0: $modelId0")
-        println(f"action1: $action1, modelId1: $modelId1")
-
-        var actions0 = MutableList.empty[Double]
-        var modelIds0 = MutableList.empty[Double]
-        var actions1 = MutableList.empty[Double]
-        var modelIds1 = MutableList.empty[Double]
-        while( i < 2000){
+    def getAverages():(Double, Double, Double, Double) = {
+        var (actions0, modelIds0, actions1, modelIds1)  = (ListBuffer.empty[Double], ListBuffer.empty[Double], ListBuffer.empty[Double], ListBuffer.empty[Double])
+        var j = 0
+        while( j < 1000) {
             val (action0, modelId0) = ensemble.actWithID(Array(1.0, 1.0, 0.0), ())
             val (action1, modelId1) = ensemble.actWithID(Array(1.0, 0.0, 1.0), ())
             actions0 += action0
             modelIds0 += modelId0
             actions1 += action1
             modelIds1 += modelId1
+            j += 1
+        }
+        (actions0.sum/1000, modelIds0.sum/1000, actions1.sum/1000, modelIds1.sum/1000)
+    }
+
+    def run(): Unit = {
+        println("started run")
+        var i = 0
+        var (shares0, shares1) =  (ListBuffer.empty[Double], ListBuffer.empty[Double])
+        while(i < 100){
+            if(i % 1  == 0){
+                var (share0, _, share1, _) = getAverages() 
+                shares0 += share0
+                shares1 += share1
+            }
+
+            if(i > 25){
+                ensemble.update(0, Array(1.0, 0.0, 1.0), 0.49)
+                ensemble.update(0, Array(1.0, 1.0, 0.0), 0.51)
+                ensemble.update(1, Array(1.0, 1.0, 0.0), 0.49)
+                ensemble.update(1, Array(1.0, 0.0, 1.0), 0.51)
+            } else {
+                ensemble.update(1, Array(1.0, 0.0, 1.0), 0.49)
+                ensemble.update(1, Array(1.0, 1.0, 0.0), 0.51)
+                ensemble.update(0, Array(1.0, 1.0, 0.0), 0.49)
+                ensemble.update(0, Array(1.0, 0.0, 1.0), 0.51)
+            }
             i += 1
         }
-        println("1000 actions")
-        println(f"action0: ${actions0.sum/1000}, modelId0: ${modelIds0.sum/1000}")
-        println(f"action1: ${actions1.sum/1000}, modelId1: ${modelIds1.sum/1000}")
+        println("-----finished loop------")
+
+        var (actions0, modelIds0, actions1, modelIds1) = getAverages()
+        println("last values")
+        println(f"action0: ${actions0}, modelId0: ${modelIds0}")
+        println(f"action1: ${actions1}, modelId1: ${modelIds1}")
+
+        println(Chart(1, 0, 0, 100).plotLine(shares0.toList, "0", "-").plotLine(shares1.toList, "1", "+").render())
 
 
     }
