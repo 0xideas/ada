cbf999713fdbf36dac39c89dbdd459d16b1a4765
leon <leontimnaluithlen@gmail.com>
2020-11-09 12:31:41 +0100

    added contextual epsilon greedy learning

--- build.sbt
+++ build.sbt
@@ -12,11 +12,15 @@ libraryDependencies  ++= Seq(
   "org.scalanlp" %% "breeze-natives" % "1.1",
   "org.scalanlp" %% "breeze-viz" % "1.1",
   //"com.stripe" %% "rainier-core" % "0.3.3",
-  "com.github.haifengl" %% "smile-scala" % "2.5.3"
+  "com.github.haifengl" %% "smile-scala" % "2.5.3",
+  "org.apache.logging.log4j" % "log4j-core" % "2.13.3",
+  "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.13.3",
+  "org.bytedeco" % "javacpp"   % "1.5.4" classifier "linux-x86_64",
+  "org.bytedeco" % "openblas"  % "0.3.10-1.5.4" classifier "linux-x86_64",
+  "org.bytedeco" % "arpack-ng" % "3.7.0-1.5.4" classifier "linux-x86_64"
 
 )
 
 
 testOptions in Test += Tests.Argument(TestFrameworks.JUnit, "-a", "-v", "-s")
 testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-oF")
-
--- src/main/scala/periphery/demos/PointRegressionContextDemo.scala
+++ src/main/scala/periphery/demos/PointRegressionContextDemo.scala
@@ -1,6 +1,7 @@
-package periphery.demos
+package epsilon.demos
 
 import scala.collection.mutable.{Map => MutableMap}
+import scala.collection.mutable.{ArrayBuffer => MutableList}
 
 import epsilon.core.components.distributions.PointRegressionContext
 import epsilon.core.ensembles._
@@ -14,11 +15,12 @@ object DemoPointRegressionContext{
     val model0 = new DummyModel(0.0)
     val model1 = new DummyModel(1.0)
 
-    val initIndependent = DataFrame.of(Array(Array.fill(2)(0.0), Array.fill(2)(1.0)))
-    val initTarget = DataFrame.of(Array(Array(0.5), Array(0.5) ), "target")
+    val r = scala.util.Random
+    val initIndependent = DataFrame.of(Array.fill(50, 2){r.nextDouble})
+    val initTarget =  DataFrame.of((Array.fill(25)(0.0) ++ Array.fill(25)(1.0)).map(Array(_)), "target")
     val initData = initIndependent.merge(initTarget)
-    val regressionContext1 = new PointRegressionContext[Array[Double]]("class" ~, initData )
-    val regressionContext2 = new PointRegressionContext[Array[Double]]("class" ~, initData )
+    val regressionContext1 = new PointRegressionContext[Array[Double]]("target" ~, initData )
+    val regressionContext2 = new PointRegressionContext[Array[Double]]("target" ~, initData )
 
     val ensemble = new EpsilonEnsembleGreedySoftmaxLocalWithContext[Int, Array[Double], Unit, Double, PointRegressionContext[Array[Double]]](
         Map(0 -> model0, 1 -> model1),
@@ -30,16 +32,41 @@ object DemoPointRegressionContext{
     )
 
     def run(): Unit = {
+        println("started run")
         var i = 0
         while(i < 1000){
-            ensemble.update(0, Array(0.0, 1.0), 0.0)
-            ensemble.update(0, Array(1.0, 1.0), 1.0)
+            ensemble.update(0, Array(1.0, 0.0, 1.0), 0.0)
+            ensemble.update(0, Array(1.0, 1.0, 0.0), 1.0)
 
-            ensemble.update(1, Array(0.0, 1.0), 0.0)
-            ensemble.update(1, Array(1.0, 1.0), 1.0)
-
+            ensemble.update(1, Array(1.0, 1.0, 0.0), 0.0)
+            ensemble.update(1, Array(1.0, 0.0, 1.0), 1.0)
             i += 1
         }
+        println("finished loop")
+
+        val (action0, modelId0) = ensemble.actWithID(Array(1.0, 1.0, 0.0), ())
+        val (action1, modelId1) = ensemble.actWithID(Array(1.0, 0.0, 1.0), ())
+        println("single action")
+        println(f"action0: $action0, modelId0: $modelId0")
+        println(f"action1: $action1, modelId1: $modelId1")
+
+        var actions0 = MutableList.empty[Double]
+        var modelIds0 = MutableList.empty[Double]
+        var actions1 = MutableList.empty[Double]
+        var modelIds1 = MutableList.empty[Double]
+        while( i < 2000){
+            val (action0, modelId0) = ensemble.actWithID(Array(1.0, 1.0, 0.0), ())
+            val (action1, modelId1) = ensemble.actWithID(Array(1.0, 0.0, 1.0), ())
+            actions0 += action0
+            modelIds0 += modelId0
+            actions1 += action1
+            modelIds1 += modelId1
+            i += 1
+        }
+        println("1000 actions")
+        println(f"action0: ${actions0.sum/1000}, modelId0: ${modelIds0.sum/1000}")
+        println(f"action1: ${actions1.sum/1000}, modelId1: ${modelIds1.sum/1000}")
+
 
     }
 }
