dac73532f7fc4f3534c095a4b71d7d50d6049610
leon <leontimnaluithlen@gmail.com>
2020-12-18 13:07:09 +0100

    added OnnxRegression and OnnxClassifier

--- ada-core/src/main/scala/components/models/OnnxModel.scala
+++ ada-core/src/main/scala/components/models/OnnxModel.scala
@@ -14,7 +14,7 @@ import scala.jdk.CollectionConverters._
 import ai.onnxruntime.OrtUtil
 
 
-class OnnxModel[ModelID, ModelDataElement, ModelAction](path: String)
+class OnnxRegression[ModelID, ModelDataElement, ModelAction](path: String, input_name: String)
     extends StackableModel[ModelID, Array[Array[ModelDataElement]], ModelAction]
     with StackableModel2[ModelID, Array[Array[ModelDataElement]], ModelAction]{
 
@@ -24,10 +24,38 @@ class OnnxModel[ModelID, ModelDataElement, ModelAction](path: String)
 
     def act(data: Array[Array[ModelDataElement]]): ModelAction = {
         val inputs = OnnxTensor.createTensor(env, data)
-        val result = session.run(Map("float_input" -> inputs).asJava)
-
+        val result = session.run(Map(input_name -> inputs).asJava)
         //predictions
         result.get(0).getValue().asInstanceOf[Array[Array[ModelAction]]](0)(0)
+    }
+
+    def update(modelIds: List[ModelID], data: Array[Array[ModelDataElement]], reward: Reward): Unit =  ()
+    def update(modelIds: List[ModelID], reward: Reward): Unit =  ()
+
+    def actWithID(data: Array[Array[ModelDataElement]], selectedIds: List[ModelID]): (ModelAction, List[ModelID]) = 
+        (act(data), selectedIds)
+
+    def export: Json = Json.fromString(loadingTime + "   " + path)
+
+}
+
+class OnnxClassifier[ModelID, ModelDataElement, ModelAction]
+    (path: String,
+    input_name: String,
+    modelActionFn: Int => ModelAction)
+    extends StackableModel[ModelID, Array[Array[ModelDataElement]], ModelAction]
+    with StackableModel2[ModelID, Array[Array[ModelDataElement]], ModelAction]{
+
+    val env = OrtEnvironment.getEnvironment();
+    val session = env.createSession(path ,new OrtSession.SessionOptions());
+    val loadingTime = java.time.LocalDateTime.now().toString()
+
+    def act(data: Array[Array[ModelDataElement]]): ModelAction= {
+        val inputs = OnnxTensor.createTensor(env, data)
+        val result = session.run(Map(input_name -> inputs).asJava)
+        //predictions
+        val probabilities = result.get(0).getValue().asInstanceOf[Array[Array[Float]]](0)
+        modelActionFn(probabilities.indexOf(probabilities.max))
     }
 
     def update(modelIds: List[ModelID], data: Array[Array[ModelDataElement]], reward: Reward): Unit =  ()
--- ada-core/src/main/scala/ensembles/stackable/PassiveThompsonSampling.scala
+++ ada-core/src/main/scala/ensembles/stackable/PassiveThompsonSampling.scala
@@ -5,18 +5,20 @@ import ada.core.interface._
 import ada.core.components.selectors._
 import ada.core.components.distributions._
 import breeze.stats.distributions.Beta
+import ada.core.interface.PassiveEnsemble
 
 
-class PassiveGreedyEnsemble[ModelID, ModelData, ModelAction, AggregateReward <: SimpleDistribution]
+class PassiveThompsonSamplingEnsemble
+    [ModelID, ModelData, ModelAction, Distr <: SimpleDistribution]
     (models: ModelID  => StackableModel[ModelID, ModelData, ModelAction],
      modelKeys: () => List[ModelID],
-    modelRewards: Map[ModelID, AggregateReward],
+     modelRewards: Map[ModelID, Distr],
     evaluateFn: (ModelAction, ModelAction) => Reward)
-    extends GreedyEnsemble[ModelID, ModelData, ModelAction, AggregateReward](models, modelKeys, modelRewards, 1.0)
-    with PassiveEnsemble[ModelID, ModelData, ModelAction, AggregateReward]{
+    extends GreedyEnsemble[ModelID, ModelData, ModelAction, Distr](
+        models,
+        modelKeys,
+        modelRewards,
+        0.0
+    ) with PassiveEnsemble[ModelID, ModelData, ModelAction, Distr]{
         def evaluate(action: ModelAction, optimalAction: ModelAction): Reward = evaluateFn(action, optimalAction)
 }
-
-
-
-
